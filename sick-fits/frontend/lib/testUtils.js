9b2bffdb95701d2ba331efe8395956a64119d784
Edwin Corea Bonilla <edwin.corea@gmail.com>
2018-10-14 15:54:51 -0600

    An Intro to Next.js, Tooling and Routing.

--- sick-fits/frontend/lib/testUtils.js
+++ sick-fits/frontend/lib/testUtils.js
@@ -4,83 +4,83 @@ import casual from 'casual';
 casual.seed(777);
 
 const fakeItem = () => ({
-  __typename: 'Item',
-  id: 'abc123',
-  price: 5000,
-  user: null,
-  image: 'dog-small.jpg',
-  title: 'dogs are best',
-  description: 'dogs',
-  largeImage: 'dog.jpg',
+    __typename: 'Item',
+    id: 'abc123',
+    price: 5000,
+    user: null,
+    image: 'dog-small.jpg',
+    title: 'dogs are best',
+    description: 'dogs',
+    largeImage: 'dog.jpg',
 });
 
 const fakeUser = () => ({
-  __typename: 'User',
-  id: '4234',
-  name: casual.name,
-  email: casual.email,
-  permissions: ['ADMIN'],
-  orders: [],
-  cart: [],
+    __typename: 'User',
+    id: '4234',
+    name: casual.name,
+    email: casual.email,
+    permissions: ['ADMIN'],
+    orders: [],
+    cart: [],
 });
 
 const fakeOrderItem = () => ({
-  __typename: 'OrderItem',
-  id: casual.uuid,
-  image: `${casual.word}.jpg`,
-  title: casual.words(),
-  price: 4234,
-  quantity: 1,
-  description: casual.words(),
+    __typename: 'OrderItem',
+    id: casual.uuid,
+    image: `${casual.word}.jpg`,
+    title: casual.words(),
+    price: 4234,
+    quantity: 1,
+    description: casual.words(),
 });
 
 const fakeOrder = () => ({
-  __typename: 'Order',
-  id: 'ord123',
-  charge: 'ch_123',
-  total: 40000,
-  items: [fakeOrderItem(), fakeOrderItem()],
-  createdAt: '2018-04 - 06T19: 24: 16.000Z',
-  user: fakeUser(),
+    __typename: 'Order',
+    id: 'ord123',
+    charge: 'ch_123',
+    total: 40000,
+    items: [fakeOrderItem(), fakeOrderItem()],
+    createdAt: '2018-04 - 06T19: 24: 16.000Z',
+    user: fakeUser(),
 });
 
 const fakeCartItem = overrides => ({
-  __typename: 'CartItem',
-  id: 'omg123',
-  quantity: 3,
-  item: fakeItem(),
-  user: fakeUser(),
-  ...overrides,
+    __typename: 'CartItem',
+    id: 'omg123',
+    quantity: 3,
+    item: fakeItem(),
+    user: fakeUser(),
+    ...overrides,
 });
 
 // Fake LocalStorage
 class LocalStorageMock {
-  constructor() {
-    this.store = {};
-  }
+    constructor() {
+        this.store = {};
+    }
 
-  clear() {
-    this.store = {};
-  }
+    clear() {
+        this.store = {};
+    }
 
-  getItem(key) {
-    return this.store[key] || null;
-  }
+    getItem(key) {
+        return this.store[key] || null;
+    }
 
-  setItem(key, value) {
-    this.store[key] = value.toString();
-  }
+    setItem(key, value) {
+        this.store[key] = value.toString();
+    }
 
-  removeItem(key) {
-    delete this.store[key];
-  }
+    removeItem(key) {
+        delete this.store[key];
+    }
 }
 
 export {
-  LocalStorageMock,
-  fakeItem,
-  fakeUser,
-  fakeCartItem,
-  fakeOrder,
-  fakeOrderItem,
+    LocalStorageMock,
+    fakeItem,
+    fakeUser,
+    fakeCartItem,
+    fakeOrder,
+    fakeOrderItem,
 };
--- sick-fits/frontend/lib/withData.js
+++ sick-fits/frontend/lib/withData.js
@@ -3,17 +3,17 @@ import ApolloClient from 'apollo-boost';
 import { endpoint } from '../config';
 
 function createClient({ headers }) {
-  return new ApolloClient({
-    uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
-    request: operation => {
-      operation.setContext({
-        fetchOptions: {
-          credentials: 'include',
+    return new ApolloClient({
+        uri: process.env.NODE_ENV === 'development' ? endpoint : endpoint,
+        request: operation => {
+            operation.setContext({
+                fetchOptions: {
+                    credentials: 'include',
+                },
+                headers,
+            });
         },
-        headers,
-      });
-    },
-  });
+    });
 }
 
 export default withApollo(createClient);
